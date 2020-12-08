13274e848e7f1b8c198d1d95f2425e70e91014f2
kosmas1991 <kosmasgou@gmail.com>
2020-12-08 19:21:52 +0200

    nothing

--- src/Fx.java
+++ src/Fx.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -15,23 +15,33 @@ public class Fx extends JFrame {
         jTextArea = new JTextArea();
         jTextArea.setText("Not connected");
         jTextArea.setSize(100,30);
+        jTextArea.setEditable(false);
         jTextArea.setVisible(true);
         super.add(button);
         super.add(jTextArea);
         super.setSize(300,100);
         super.setLayout(new FlowLayout());
         super.setVisible(true);
-        button.addActionListener(new ActionListener() {
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jTextArea.addFocusListener(new FocusListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                jTextArea.setText("Try to connect");
-                try {
-                    Socket socket =new Socket("localhost",8080);
-                    jTextArea.setText("Connected succesfully!!");
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                    jTextArea.setText("Cannot connect to the server");
-                }
+            public void focusGained(FocusEvent e) {
+                jTextArea.setText("Ti leei bro?");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                jTextArea.setText("Pou pas?");
+            }
+        });
+        button.addActionListener(e -> {
+            jTextArea.setText("Trying to connect...");
+            try {
+                Socket socket =new Socket("localhost",8080);
+                jTextArea.setText("Connected successfully!!");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+                jTextArea.setText("Cannot connect to the server.");
             }
         });
     }
--- src/Server.java
+++ src/Server.java
@@ -5,7 +5,10 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(8080);
-        Socket socket = server.accept();
-        System.out.println("Client connected!!");
+        Socket socket;
+        for (int count = 1; count < 100; count++) {
+            socket = server.accept();
+            System.out.println("Client " + count + " connected!!");
+        }
     }
 }
